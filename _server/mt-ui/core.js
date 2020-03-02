dfa1c2bbacfb244708de95e6cce57d112b6a438a
tocque <364004564@qq.com>
2020-03-02 17:50:19 +0800

    refacting index file

--- _server/mt-ui/core.js
+++ _server/mt-ui/core.js
@@ -1,15 +1,13 @@
-import './control.js';
-
-import * as navi from'./navigation.js';
-for (let c in navi) {
-    Vue.component(navi[c].name, navi[c]);
-}
-
+import * as layout from'./layout.js';
+import * as control from'./control.js';
+import * as navigation from'./navigation.js';
 import * as others from'./others.js';
-for (let c in others) {
-    Vue.component(others[c].name, others[c]);
-}
 
 import './contextmenu.js';
 import './notify.js';
 import './tree.js';
+
+const components = { ...layout, ...control, ...navigation, ...others };
+for (let c in components) {
+    Vue.component(components[c].name, components[c]);
+}
